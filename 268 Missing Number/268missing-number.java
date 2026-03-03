566f08804d7bdbaf3eb07e33436e0885490ddea1
Bandirala Lohitha <lohithab910@gmail.com>
2026-03-03 12:00:00 +0530

    Code challenge submission for question: 268 Missing Number

---
+++ 268 Missing Number/268missing-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n=nums. length;
+         int totalSum = n* (n + 1) / 2;
+        int arraySum = 0;
+        
+        for (int num : nums) {
+            arraySum += num;
+        }
+        
+        return totalSum - arraySum;
+   
+    }
+}
