5b29c0678ebe84a07871aa9b09a80af39792be56
Bandirala Lohitha <lohithab910@gmail.com>
2026-04-03 12:00:00 +0530

    Code challenge submission for question: 2413 Smallest Even Multiple

---
+++ 2413 Smallest Even Multiple/2413smallest-even-multiple.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int smallestEvenMultiple(int n) {
+      return lcm(2,n);
+    }
+    public int lcm(int a,int b){
+        return (a*b)/gcd(a,b);
+        
+    }
+    public int gcd(int a,int b){
+        while(b!=0){
+            int temp=b;
+            b=a%b;
+            a=temp;
+        }
+        return a;
+    }
+    
+}
