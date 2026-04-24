4827eb8cb05433501b41327ce738576b241c32f5
Bandirala Lohitha <lohithab910@gmail.com>
2026-04-24 12:00:00 +0530

    Code challenge submission for question: 238 Product of Array Except Self

---
+++ 238 Product of Array Except Self/238product-of-array-except-self.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def productExceptSelf(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        n=len(nums)
+        suffix=[1]*n
+        prefix=[1]*n
+        for i in range(1,n):
+            prefix[i]=prefix[i-1]*nums[i-1]
+         
+        for i in range(n-2,-1,-1):
+            suffix[i]=suffix[i+1]*nums[i+1]
+        ans=[]
+        for i in range(n ):
+             ans.append(suffix[i]*prefix[i])
+            
+        return ans
+           
+            
+            
+            
+            
