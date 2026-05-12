4a8f3ea56363464b5d357f9e805a5a3f77f8948a
Bandirala Lohitha <lohithab910@gmail.com>
2026-05-12 12:00:00 +0530

    Code challenge submission for question: 2444 Count Subarrays With Fixed Bounds

---
+++ 2444 Count Subarrays With Fixed Bounds/2444count-subarrays-with-fixed-bounds.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    long long countSubarrays(vector<int>& nums, int minK, int maxK) {
+        long long int res=0;
+  int mini=-1,maxi=-1,badi=-1,n=nums.size();
+     
+  for(int i=0;i<n;i++){
+  if(nums[i]<minK||nums[i]>maxK){
+      badi=i;
+    }
+ if(nums[i]==minK){
+       mini=i;}
+if(nums[i]==maxK){
+        maxi=i;
+        }
+      
+      res+=max(0,min(mini,maxi)-badi);
+      }
+      return  res;
+        }
+};
+        
