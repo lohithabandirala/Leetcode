36510c07d6b033340ce939b0a292a9c46c4b00e5
Bandirala Lohitha <lohithab910@gmail.com>
2026-05-13 12:00:00 +0530

    Code challenge submission for question: 58 Length of Last Word

---
+++ 58 Length of Last Word/58length-of-last-word.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+       int l=0,i=s.length()-1;
+       while(i>=0&&s.charAt(i)==' '){
+           i--;
+           }
+        while(i>=0&&s.charAt(i)!=' '){
+            l++;
+            i--;
+    }
+        return l;
+       }
+}
